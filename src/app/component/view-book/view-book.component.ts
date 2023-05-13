5fd94980087080556c1130f082164b3fabf82e10
Romil003 <romilghadge33@gmail.com>
2023-05-13 12:25:23 +0530

    Added : editing view book page

--- src/app/component/view-book/view-book.component.ts
+++ src/app/component/view-book/view-book.component.ts
@@ -10,20 +10,32 @@ import { DataService } from 'src/app/Service/Data/data.service';
 export class ViewBookComponent implements OnInit {
 
   getBookData : any;
+  addCart : boolean = true;
+  count = 1;
 
   constructor(private route : Router,private dataService : DataService){}
   ngOnInit(){
     this.gettingDataOfOneBook()
   }
 
-  goToHome(){
-    this.route.navigateByUrl('/dashboard');
-  }
-
   gettingDataOfOneBook(){
     this.dataService.currentSource.subscribe((result : any) => {
       this.getBookData = result;
     })
+  }
+
+  addTocart(){
+    console.log("Adding book to cart");
+    this.addCart = false;
+  }
+
+  dec(){
+    
+    this.count = this.count - 1;
+  }
+
+  inc(){
+    this.count = this.count + 1;
   }
 
 }
